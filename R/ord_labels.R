41328953d652a9dad85c3ba08238dc5fee468865
John Quensen <quensenj@msu.edu>
2020-04-25 08:34:54 -0400

    fix ord labels when no eigenvalues

--- R/ord_labels.R
+++ R/ord_labels.R
@@ -15,26 +15,31 @@
 ord_labels <-
   function(ord){
     ev <- vegan::eigenvals(ord)
-    tol <- -(1e-07)*ev[1]
-    ord.labels <- rep("", length(ev))
-    if ((any(is.na(ev))) | (any(ev < tol))) {
-      for ( i in 1:length(ev)) {
-        ord.labels[i] <- paste("DIM", i, sep = "")
-      }
-    }
-    else {
-      ev.pc <- round(100*(ev/sum(ev)), 2)
-      axis.names <- names(ev)
-      if (is.null(axis.names)) {
-        for ( i in 1:length(ev.pc)) {
-          ord.labels[i] <- paste("DIM", i, " ", sprintf(ev.pc[i], fmt = '%#.1f'), "%", sep="")
-        }
-      } else {
-        for (i in 1:length(ev.pc)){
-          ord.labels[i] <- paste(axis.names[i], " ", ev.pc[i],"%", sep="")
+    if (!is.na(ev)) {
+      tol <- -(1e-07)*ev[1]
+      ord.labels <- rep("", length(ev))
+      if ((any(is.na(ev))) | (any(ev < tol))) {
+        for ( i in 1:length(ev)) {
+          ord.labels[i] <- paste("DIM", i, sep = "")
         }
       }
+      else {
+        ev.pc <- round(100*(ev/sum(ev)), 2)
+        axis.names <- names(ev)
+        if (is.null(axis.names)) {
+          for ( i in 1:length(ev.pc)) {
+            ord.labels[i] <- paste("DIM", i, " ", sprintf(ev.pc[i], fmt = '%#.1f'), "%", sep="")
+          }
+        } else {
+          for (i in 1:length(ev.pc)){
+            ord.labels[i] <- paste(axis.names[i], " ", ev.pc[i],"%", sep="")
+          }
+        }
+      }
+    } else {
+      ord.labels <- colnames(vegan::scores(ord))
     }
+
     return(ord.labels)
   }
 
